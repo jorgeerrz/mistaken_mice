a5daa5acae431c8208ae3867e1650f844e562502
SDAMcIntyre <sdamcintyre+git@gmail.com>
2020-07-21 19:37:40 +0200

    commented out __main__ code
    colabs complains about indentation of this section for some reason

--- summarise_dataset.py
+++ summarise_dataset.py
@@ -33,33 +33,33 @@ def summarise_dataset(dataset):
 
 if __name__ == "__main__":
 
-	# Data retrieval
-	import os, requests
+	# # Data retrieval
+	# import os, requests
 
-	fname = []
-	for j in range(3):
-	  fname.append('steinmetz_part%d.npz'%j)
-	url = ["https://osf.io/agvxh/download"]
-	url.append("https://osf.io/uv3mw/download")
-	url.append("https://osf.io/ehmw2/download")
+	# fname = []
+	# for j in range(3):
+	  # fname.append('steinmetz_part%d.npz'%j)
+	# url = ["https://osf.io/agvxh/download"]
+	# url.append("https://osf.io/uv3mw/download")
+	# url.append("https://osf.io/ehmw2/download")
 
-	for j in range(len(url)):
-		if not os.path.isfile(fname[j]):
-			try:
-				r = requests.get(url[j])
-			except requests.ConnectionError:
-				print("!!! Failed to download data !!!")
-			else:
-				if r.status_code != requests.codes.ok:
-					print("!!! Failed to download data !!!")
-			else:
-				with open(fname[j], "wb") as fid:
-				fid.write(r.content)
+	# for j in range(len(url)):
+		# if not os.path.isfile(fname[j]):
+			# try:
+				# r = requests.get(url[j])
+			# except requests.ConnectionError:
+				# print("!!! Failed to download data !!!")
+			# else:
+				# if r.status_code != requests.codes.ok:
+					# print("!!! Failed to download data !!!")
+			# else:
+				# with open(fname[j], "wb") as fid:
+				# fid.write(r.content)
   
-	# Data loading
-	alldat = np.array([])
-	for j in range(len(fname)):
-	  alldat = np.hstack((alldat, np.load('steinmetz_part%d.npz'%j, allow_pickle=True)['dat']))
+	# # Data loading
+	# alldat = np.array([])
+	# for j in range(len(fname)):
+	  # alldat = np.hstack((alldat, np.load('steinmetz_part%d.npz'%j, allow_pickle=True)['dat']))
 	
-	# summarise the dataset
-	summarise_dataset(alldat)
+	# # summarise the dataset
+	# summarise_dataset(alldat)
