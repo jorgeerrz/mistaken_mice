a91654b0ea7c1e9157882b6ddcd99e3eaed94074
Gittinator <henry.m.skelton@gmail.com>
2020-07-22 11:07:49 -0400

    Update filter.py
    made area filtering optional, return dictionary instead of tuple (to be compatible with downstream functions)

--- filter.py
+++ filter.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 #this function will do the filtering
-def filter_spikes(alldata, session_id):
+def filter_spikes(alldata, session_id,brain_area=None):
     #alldata is the collated data from all sessions/neurons/timepoints as shown in the tutorial notebook
     #grab spikes/choices from one section
     dat = alldata[session_id]
@@ -17,20 +17,22 @@ def filter_spikes(alldata, session_id):
     RTs = RTs[unfair_chosey_filter]
 
     #grab only spikes from the VISp
-    spks = spks[dat['brain_area']=='VISp',:,:]
+    if brain_area:
+        spks = spks[dat['brain_area']==brain_area,:,:]
 
     #grab only spikes from between -500ms and 500ms, relative to stimulus onset (each bin is 10ms)
     spks = spks[:,:,0:100]
 
-    return spks, chcs, RTs
+    return {'spks':spks, 'chcs':chcs, 'RTs':RTs}
+
 
 def filter_dataset(alldata):
     #alldata is the collated data from all sessions/neurons/timepoints as shown in the tutorial notebook
     #grab spikes/choices from one section
     alldata_f = np.zeros_like(alldata)
-    for session_id in range(alldata.shape[0]):       
+    for session_id in range(alldata.shape[0]):
         spikes_f, chcs_f, RTs_f = filter_spikes(alldata,session_id)
-        dict_f = {"spks" : spikes_f, "response": chcs_f, "response_time": RTs_f, 
+        dict_f = {"spks" : spikes_f, "response": chcs_f, "response_time": RTs_f,
         "brain_area": alldata[session_id]['brain_area'], "bin_size": alldata[session_id]['bin_size']}
 
         alldata_f[session_id] = dict_f
