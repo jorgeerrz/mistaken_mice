2f9c4fe96c6fb1db05c5d58ea437f316e5666325
jorge <jorge.ramirez@upf.edu>
2020-07-22 15:31:14 +0200

    Updated filter.py to include filtering of dataset

--- filter.py
+++ filter.py
@@ -4,7 +4,7 @@ import numpy as np
 def filter_spikes(alldata, session_id):
     #alldata is the collated data from all sessions/neurons/timepoints as shown in the tutorial notebook
     #grab spikes/choices from one section
-    dat = alldat[session_id]
+    dat = alldata[session_id]
     spks = dat['spks']
     chcs = dat['response']
     RTs = dat['response_time']
@@ -23,3 +23,16 @@ def filter_spikes(alldata, session_id):
     spks = spks[:,:,0:100]
 
     return spks, chcs, RTs
+
+def filter_dataset(alldata):
+    #alldata is the collated data from all sessions/neurons/timepoints as shown in the tutorial notebook
+    #grab spikes/choices from one section
+    alldata_f = np.zeros_like(alldata)
+    for session_id in range(alldata.shape):       
+        spikes_f, chcs_f, RTs_f = filter_spikes(alldata,session_id)
+        dict_f = {"spks" : spikes_f, "response": chcs_f, "response_time": RTs_f, 
+        "brain_area": alldata[session_id]['brain_area'], "bin_size": alldata[session_id]['bin_size']}
+
+        alldata_f[session_id] = dict_f
+
+    return alldata_f
