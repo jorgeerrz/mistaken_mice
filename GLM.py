7ac24e5ccd219327d2c4607c79e8ea8a869e6e4f
jorge <jorge.ramirez@upf.edu>
2020-07-27 18:03:11 +0200

    modified rebin and included a new PCA function
    neurons_time_PCA function takes in neurons,trials,time and reshapes
    
    rebin now inputs a dict and has the option of defining minT and maxT

--- GLM.py
+++ GLM.py
@@ -78,4 +78,69 @@ def GLM_logistic(features, choices, pen = "l2", lambda_L2 = 1):
 
   return log_reg
 
+def neurons_time_PCA(dat,tVar,minT,maxT,toplot=False):
+  #Perform PCA with the neurons of a single session. Components are estimated by considering activity in the range minT:maxT
+  #Example input neurons_PCA(dat,0.9,51,130)
+
+  import numpy as np
+  import matplotlib.pyplot as plt
+  from sklearn.decomposition import PCA
+
+  NT = dat['spks'].shape[-1]
+
+  #@title top PC directions from stimulus + response period, with projections of the entire duration
+  NN = dat['spks'].shape[0]
+  #All trials are concatenated to result in a session-long time series for each neuron:
+  #droll = np.reshape(dat['spks'][:,:,minT:maxT], (NN,-1)) # first 80 bins = 1.6 sec
+  #(N.B. only the time bins for stimulus + response are used!)
+
+  #droll = droll - np.mean(droll, axis=1)[:, np.newaxis] #center each neuron's response vector
+  #(np.newaxis is used to add an additional dimension, to be consistent with the original droll matrix)
+
+  droll = reshaping_features(dat['spks']) #[:,:,minT:maxT].mean(axis=2)
+  droll = droll.T
+  nPCs = min(droll.shape) #set how many PCs we are interested in (all of them)
+
+  model = PCA(n_components = nPCs).fit(droll.T) #perform PCA!
+  weights = model.components_ #extract the weight of each PC dimension for each neuron
+  PCneurons = weights @ droll #multiply each neuron by its corresponding weights
+  #(N.B. the entire trial durations for each neuron are multiplied by the weight! But weight were extracted only from a portion)
+  #PCneurons = np.reshape(PCneurons, (nPCs, -1, NT)) #session-long time series are split again into trial-long time series
+
+  explVar = model.explained_variance_
+  
+
+  fracVar = explVar/np.sum(explVar) #fraction of explained variance for each PC
+  cumVar = np.cumsum(fracVar) #cumulative sum of explained variance
+  PCrange = np.max(np.where(cumVar<=tVar)) #The lowest number of components that explain the threshold variance
+  
+  if toplot == True:
+      plt.figure()
+      plt.bar(range(nPCs),explVar)
+      plt.xlabel('Eigenvector')
+      plt.ylabel('Explained variance')
+      
+      plt.plot([PCrange,PCrange],[0,np.max(explVar)],'r')
+
+      #@title The top PCs capture most variance across the brain. What do they care about?
+      plt.figure(figsize= (20, 6))
+  
+      for iPC in range((PCneurons).shape[0]):
+
+        this_pc = PCneurons[iPC]
+
+        plt.plot(this_pc.mean(axis=0))
+
+        if iPC==0:
+          plt.legend(['right only', 'left only', 'neither', 'both'], fontsize=8)
+          plt.xlabel('binned time')
+          plt.ylabel('Component value')
+        # plt.title('PC %d'%j)
+
+  dat['PCs'] = PCneurons
+  dat['weights'] = weights
+  dat['PCrange'] = PCrange+1
+
+  return dat
+
 
--- rebin.py
+++ rebin.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def rebin(input, newbin, oldbin = 0.01):
+def rebin(dat, minT, maxT, newbin, oldbin = 0.01):
   ''' takes spike counts from 'spks' and rebins them into a new time bin specified by newbin
 
   Args:
-  input = three-dimensional data array with spike counts (dat['spks'], dimensions: neurons * trials * time bins)
+  input = dict for the session (dat['spks'], dimensions: neurons * trials * time bins)
   newbin = size of the new time bin in seconds
   oldbin = size of old time bin. default 0.01s
 
@@ -13,8 +13,8 @@ def rebin(input, newbin, oldbin = 0.01):
   newbin = size of new bin size
   '''
 
-  assert newbin > oldbin 'new bin size must be bigger than old bin size'
-
+  #assert newbin > oldbin 'new bin size must be bigger than old bin size'
+  input = dat['spks'][:,:,minT:maxT]
   # initialize variables
   poolbin = newbin/oldbin # calculates numbers of bins to be pooled into one new bin
   newcount = np.zeros((input.shape[0],input.shape[1],int(input.shape[2]/poolbin))) # create matrix of zeros with appropriate third dimension (number of bins)
@@ -25,5 +25,8 @@ def rebin(input, newbin, oldbin = 0.01):
       # sum up counts from one step to the next
       slc = input[:,:,int(idx*poolbin):int((idx+1)*poolbin)]
       newcount[:,:,idx] = np.sum(slc, axis=2)
+  
+  dat2 = dat.copy()
+  dat2['spks'] = newcount
 
-  return newcount, newbin
+  return dat2 #newcount, newbin
